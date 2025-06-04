2c33b97d421dcf4e6c2f3b3a62d90d8649aba7f3
Dan13l06 <167872814+Dan13l06@users.noreply.github.com>
2025-06-04 09:26:47 +0200

    Updated the SVM pipeline and made evalution prettier

--- pipelines/pipeline_SVM.py
+++ pipelines/pipeline_SVM.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from whale_call_project.preprocessing.preprocess_SVM import Preprocess, evaluate_model
 from whale_call_project.metrics.visualization import Visualizer
 import numpy as np
@@ -12,19 +15,35 @@ if __name__ == "__main__":
     # test_predict = preprocessor.preprocess_validation_test_files("data/test_data")
 
     training_data = np.load("data/x_train.npy")
-    train_dataset = pd.read_csv("data/training_data.csv")
+    train_dataset = pd.read_csv("data/data_labels/training_data.csv")
     preprocessor.pipeline.fit(training_data, train_dataset["label"])
 
     val_list = np.load("data/validation_data_list.npy")
-    val_dataset = pd.read_csv("data/validation_data.csv")
+    val_dataset = pd.read_csv("data/data_labels/validation_data.csv")
     val_predict = preprocessor.pipeline.predict(val_list)
 
     test_list = np.load("data/test_data_list.npy")
-    test_dataset = pd.read_csv("data/test_data.csv")
+    test_dataset = pd.read_csv("data/data_labels/test_data.csv")
     test_predict = preprocessor.pipeline.predict(test_list)
 
-    print(evaluate_model("classification_report", val_dataset["label"], val_predict))
-    print(evaluate_model("classification_report", test_dataset["label"], test_predict))
+
+    print("Evaluation on Training Set:")
+    print("-" * 50)
+    print("Precision: ", evaluate_model("precision", val_dataset["label"], val_predict))
+    print("Recall: ", evaluate_model("recall", val_dataset["label"], val_predict))
+    print("F1_score: ", evaluate_model("f1_score", val_dataset["label"], val_predict))
 
     Visualizer().plot_confusion_matrix(val_predict, val_dataset["label"], "Validation")
+
+    print()
+
+    print("Evaluation on Test Set:")
+    print("-" * 50)
+    print("Precision: ", evaluate_model("precision", test_dataset["label"], test_predict))
+    print("Recall: ", evaluate_model("recall", test_dataset["label"], test_predict))
+    print("F1_score: ", evaluate_model("f1_score", test_dataset["label"], test_predict))
+
     Visualizer().plot_confusion_matrix(test_predict, test_dataset["label"], "Test")
+
+    print("-" * 50)
+
